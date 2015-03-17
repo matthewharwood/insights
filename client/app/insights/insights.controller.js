d393d4b7c98b28f2d60d79fefabd05be2630114f
Thomas Slater <Thomas.Slater@akqa.com>
2015-03-17 11:18:30 -0700

    canvas event handler and delineator

--- client/app/insights/insights.controller.js
+++ client/app/insights/insights.controller.js
@@ -7,6 +7,65 @@ angular.module('keystoneApp')
     var token = "T1==cGFydG5lcl9pZD00NTE4MjQ0MiZzaWc9MGRjY2Q5MjYxODY3NmVkNDc4MGY4OGM5YTQ5YjM0OWFjZDA1YTEyZTpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5URTRNalEwTW41LU1UUXlOalUwTlRZM056UTRNWDVGTHpsTVdUTkZOalk0VEVOd2FFTk9XRTB2VjBad01WTi1mZyZjcmVhdGVfdGltZT0xNDI2NTQ1NzEzJm5vbmNlPTAuNTMzMzIxNDUyNjUxODM4NQ==";
     var session = OT.initSession(apiKey, sessionId);
     var isAdmin = Auth.isAdmin();
+    var canvas = document.getElementById('touchCanvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    var boundaries = [
+      {
+        startX: window.innerWidth/3,
+        startY: 0,
+        endX: window.innerWidth/3,
+        endY: window.innerHeight
+      },
+      {
+        startX: window.innerWidth - (window.innerWidth/3),
+        startY: 0,
+        endX: window.innerWidth - (window.innerWidth/3),
+        endY: window.innerHeight
+      },
+      {
+        startX: 0,
+        startY: window.innerHeight /3,
+        endX: window.innerWidth,
+        endY: window.innerHeight /3
+      },
+      {
+        startX: 0,
+        startY: window.innerHeight - (window.innerHeight /3),
+        endX: window.innerWidth,
+        endY: window.innerHeight - (window.innerHeight / 3)
+      }
+    ];
+
+    /*ctx.beginPath();
+    ctx.moveTo(boundaries[0].startX,boundaries[0].startY);
+    ctx.lineTo(boundaries[0].endX,boundaries[0].endY);
+    ctx.stroke();
+    ctx.moveTo(boundaries[1].startX,boundaries[1].startY);
+    ctx.lineTo(boundaries[1].endX,boundaries[1].endY);
+    ctx.stroke();
+    ctx.moveTo(boundaries[2].startX,boundaries[2].startY);
+    ctx.lineTo(boundaries[2].endX,boundaries[2].endY);
+    ctx.stroke();
+    ctx.moveTo(boundaries[3].startX,boundaries[3].startY);
+    ctx.lineTo(boundaries[3].endX,boundaries[3].endY);
+    ctx.stroke();*/
+
+
+    $(canvas).on('click', clickHandler);
+
+    function clickHandler(e){
+      //e.preventDefault();
+      if (e.clientX < boundaries[0].startX){
+        $scope.sendMQTT(2);
+      } else if (e.clientX > boundaries[1].startX){
+        $scope.sendMQTT(0);
+      } else if (e.clientX > boundaries[0].startX && e.clientX < boundaries[1].startX){
+        $scope.sendMQTT(1);
+      }
+    }
 
 
     session.on({
