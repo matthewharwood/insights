e1286fe5bbc9b6ca2b1082edb3596861d57b45f1
Thomas Slater <Thomas.Slater@akqa.com>
2015-03-31 11:14:52 -0700

    throwing in controller logic for on the fly voice changes Speech.speak can now take 3 args, 1. message 2. gender, 3. origin (but really, only for England and Germany)

--- client/app/latest/latest.controller.js
+++ client/app/latest/latest.controller.js
@@ -7,23 +7,23 @@ angular.module('keystoneApp')
      $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
 
-// 0. init default 
+// 0. init default
       socket.syncUpdates('thing', $scope.awesomeThings, function(){
           $scope.selectedIndex = $scope.awesomeThings.length-1;
           console.log($scope.awesomeThings[$scope.selectedIndex], 'no', !$scope.awesomeThings[0].power)
           console.log($scope.awesomeThings, 'updated things in latestctrl');
 
           if(typeof $scope.awesomeThings[$scope.selectedIndex].power !== 'undefined' && $scope.awesomeThings[$scope.selectedIndex].power ) {
-            Speech.speak('Hello World, My name is Steven Brick, and I am from London');
+            Speech.speak('Hello World, My name is Steven Brick, and I am from London', 'male', 'england');
           }else if(typeof $scope.awesomeThings[0].power !== 'undefined' && !$scope.awesomeThings[0].power) {
-            Speech.speak('Hello Sebastian, My name is Steven, pleased to meet you');
+            Speech.speak('Hello Sebastian, My name is Steven, pleased to meet you', 'male', 'england');
           } else {
             return false;
           }
 
-          
-        
+
+
       });
-   
+
     });
   });
