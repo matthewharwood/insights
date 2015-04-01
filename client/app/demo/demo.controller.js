aeca798b428966bcc7395827d128a21601e0ba45
Matthew Harwood <matthhar12@gmail.com>
2015-04-01 05:10:49 -0700

    big commit fuck everything

--- client/app/demo/demo.controller.js
+++ client/app/demo/demo.controller.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('keystoneApp')
-  .controller('DemoCtrl', function ($scope, $http, $state) {
+  .controller('DemoCtrl', function ($scope, $http, $state, socket) {
   
     $scope.$state = $state;
     
     if(typeof $state.params.id === "undefined" || $state.params.id === ''){
+      console.log('heyhey')
       var count = 0;
     }
     else {
@@ -18,59 +19,65 @@ angular.module('keystoneApp')
 
         count = $state.params.id;
       }
+      if(toParams.id=== 'add'){
+        $state.go('demo.add');
+      }
+
     })
+
     $scope.audioClips = 
       [
         {
-          src: '_1givemeaname.ogg',
+          src: '_1givemeaname',
 
         },
         {
           title: 'please state a name',
-          src: '_0on.ogg',
+          src: '_0on',
           desc: 'ex. you\'re name is (name)'
         },
         {
           title: '"Steven" is that correct?',
-          src: '_0off.ogg',
+          src: '_0off',
         },
         {
           title: 'Alright, my name is',
-          src: '_2alrightimsteven.ogg',
+          src: '_2alrightimsteven',
           input: ': Steven'
         },
         {
           title: 'Please choose a gender',
-          src: '_3chooseagender.ogg'
+          src: '_3chooseagender'
         },
         {
           title: 'Please choose a gender',
-          src: '_0on.ogg',
+          src: '_0on',
           desc: 'ex. you are (gender)'
         },
         {
           title: '"Male" is that correct?',
-          src: '_0off.ogg'
+          src: '_0off'
         },
         {
           title: 'Excellent, I am Steven?',
-          src: '_4iamsteven.ogg'
+          src: '_4iamsteven'
         },
         {
           title: 'Where is he from?',
-          src: '_5whereamifrom.ogg'
+          src: '_5whereamifrom'
         },
         {
           title: 'Where is he from?',
-          src: '_0on.ogg',
+          src: '_0on',
           desc: 'ex. you\'re from (location)'
         },
         {
           title: '"London" is that correct?',
-          src: '_0off.ogg'
+          src: '_0off'
         },
         {
-          src: '_6fromlondon.ogg'
+          title: 'Excellent! I am from London',
+          src: '_6fromlondon'
         },
       ];
 
@@ -79,10 +86,7 @@ angular.module('keystoneApp')
 
     //defines count at start  
 
-    function doneSon(){
-      console.log('done');
-    }
-    
+
     $scope.nextStep = function() {
       if(count >= $scope.audioClips.length){
         $state.go('demo.add');
@@ -90,32 +94,84 @@ angular.module('keystoneApp')
       }
 
       $state.go('demo.step',{id: count});
-      $('.step-' + count)[0].play();
+      // $('.step-' + count)[0].play();
       
       count++;
     }
+    //navigation
+    //
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+      socket.syncUpdates('thing', $scope.awesomeThings, function(){});
+
+
+    });
+
+    $scope.refreshDemo = function(){
+
+      $state.go('demo',{}, {reload: true});
+    }
+
+    $scope.open = false;
+    $scope.opendrawer = function() {
+      $scope.open = !$scope.open;
+    }
+     $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('thing');
+    });
+    //end of navigation
     
     //last few steps
+    function vibrate(){
+      navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+           
+        if (navigator.vibrate) {
+            navigator.vibrate(500);
+        }
+    }
+    
     $scope.submitSteven = function() {
       $http.post('/api/things', { name: 'Steven'}).
         success(function(data, status, headers, config) {
-          $scope.awesomeThings = data;
+          vibrate();
         });
     };
 
     $scope.submitSebastian = function() {
       $http.post('/api/things', { name: 'Sebastian'}).
         success(function(data, status, headers, config) {
-          $scope.awesomeThings = data;
+          vibrate();
         });
     };
     
      $scope.introSteven = function() {
       $http.post('/api/things', { name: 'intro'}).
         success(function(data, status, headers, config) {
-          $scope.awesomeThings = data;
+          vibrate();
         });
     };
+
+    var submitCounter = 0;
+    $scope.submitData = function() {
+      switch(submitCounter){
+        case 0:
+          console.log('added steven')
+          $scope.submitSteven();
+          break;
+        case 1:
+          console.log('added seb')
+          $scope.submitSebastian();
+          break;
+        case 2:
+          console.log('introsteven')
+          $scope.introSteven();
+      }
+      submitCounter++;
+    }
 
     //end of ctrl
   });
@@ -126,12 +182,12 @@ angular.module('keystoneApp')
     restrict: 'A',
     scope: { code:'=', idx: '=' },
     replace: true,
-    template: '<audio ng-src="{{url}}" controls class="step-{{idx}}"></audio>',
+    template: '<audio controls class="step-{{idx}}"><source ng-src="{{url}}" type="audio/ogg"></audio>',
     link: function (scope) {
         console.log(scope.idx)
         scope.$watch('code', function (newVal, oldVal) {
            if (newVal !== undefined) {
-               scope.url = $sce.trustAsResourceUrl("../assets/audio/" + newVal);
+               scope.url = $sce.trustAsResourceUrl("../assets/audio/" + newVal + '.ogg');
            }
         });
     }
